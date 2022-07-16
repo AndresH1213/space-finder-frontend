bcc09ad17578bf7ff7f1d2e05df745582e25a10b
Andres Hernandez <edanhebla1213@hotmail.com>
2022-07-16 15:17:39 -0500

    components for spaces

---
+++ src/components/spaces/SpaceComponent.tsx
@@ -0,0 +1,37 @@
+import { Component, ReactNode } from 'react';
+import genericImage from '../../assets/generic-image.jpg';
+import './SpaceComponent.css';
+
+interface SpaceComponentProps {
+  spaceId: string;
+  name: string;
+  location: string;
+  photoUrl?: string;
+  reserveSpace: (spaceId: string) => void;
+}
+
+export class Spacecomponent extends Component<SpaceComponentProps> {
+  private renderImage() {
+    return this.props.photoUrl ? (
+      <img src={this.props.photoUrl} alt="" />
+    ) : (
+      <img src={genericImage} alt="" />
+    );
+  }
+  render(): ReactNode {
+    return (
+      <div className="spaceComponent">
+        {this.renderImage()}
+        <label className="name">{this.props.name}</label>
+        <br />
+        <label className="spaceId">{this.props.spaceId}</label>
+        <br />
+        <label className="location">{this.props.location}</label>
+        <br />
+        <button onClick={() => this.props.reserveSpace(this.props.spaceId)}>
+          Reserve
+        </button>
+      </div>
+    );
+  }
+}
