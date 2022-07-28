9b95d0c921ff79a841e966c384bbe12a7003c411
Andres Hernandez <edanhebla1213@hotmail.com>
2022-07-27 19:38:58 -0500

    adding model interfaces for user and spaces

---
+++ src/model/Model.ts
@@ -0,0 +1,17 @@
+import { CognitoUser } from '@aws-amplify/auth';
+export interface User {
+  username: string;
+  cognitoUser: CognitoUser;
+}
+
+export interface UserAttribute {
+  Name: string;
+  Value: string;
+}
+
+export interface Space {
+  spaceId: string;
+  name: string;
+  location: string;
+  photoURL?: string;
+}
